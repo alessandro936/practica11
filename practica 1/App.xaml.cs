009d78dc63fce3861245ec4b70881f80f21dee5a
alessandro936 <botelloalessandro97@gmail.com>
2024-08-10 14:02:07 -0500

    Initial commit

---
+++ practica 1/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using practica_1.views;
+
+namespace practica_1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new home());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
